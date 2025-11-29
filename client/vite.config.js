97fa61ddb9f9035af331f7db0510509f18916755
Muhammad kamran Kami <muhammadkamrankami313@gmail.com>
2025-11-29 17:43:23 +0500

    Integrated PWA

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true // Allows you to test PWA in npm run dev
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'EventPulse - Event Manager',
+        short_name: 'EventPulse',
+        description: 'Connect, Organize, and Celebrate with EventPulse.',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone', // Looks like a native app (no browser bar)
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
